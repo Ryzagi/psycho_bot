f72a43a23a93c05bc8c53bd5b2df1e1d2f14422d
Ryzagi <vladislavrzhv@gmail.com>
2023-06-28 18:54:21 +0300

    git init

---
+++ config.py
@@ -0,0 +1,32 @@
+DEFAULT_TEMPLATE = """I want you to act a psychologist assistant. I suggest switching to other doctors. Now there is 
+a psychotherapist on the team /psychotherapist, /doctor, /hypnotherapist. Offer to start communicating with them 
+based on user needs. Current conversation: {history} Human: {input} Medical assistant:"""
+
+PREMIUM_MESSAGE = """Premium message"""
+
+WELCOME_MESSAGE = """Hi, im Vishal psychotherapist!\nHow can I help you?"""
+
+ERROR_MESSAGE = "Hi! Press /start to start a new conversation."
+
+LIMIT_MESSAGE = """Your limit is reached. Please buy a subscription to continue."""
+
+DATA_STRUCTURE = [
+        {
+            'type': 'human',
+            'data': {
+                'content': '',
+                'additional_kwargs': {}
+            }
+        },
+        {
+            'type': 'ai',
+            'data': {
+                'content': '',
+                'additional_kwargs': {}
+            }
+        }
+    ]
+
+
+class Prompt:
+    prompt = DEFAULT_TEMPLATE

df02f23133590c24723fa21c8dcece2086a81a66
Ryzagi <vladislavrzhv@gmail.com>
2023-07-18 22:53:10 +0300

    feat: new prompt

--- app.py
+++ app.py
@@ -205,7 +205,7 @@ async def handle_message(request: Message) -> dict:
             ---------------- {summaries} 
             Current conversation: {history} 
             Human: {question} 
-            Psychotherapist (include blank lines): """
+            Psychotherapist (You must always include blank lines in text): """
             messages = [
                 SystemMessagePromptTemplate.from_template(system_template),
                 HumanMessagePromptTemplate.from_template("{question}")
